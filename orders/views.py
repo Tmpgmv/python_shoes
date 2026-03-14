cdd028e9f89a9cc3f6b34c5db981cee656e615de
Your Name <you@example.com>
2026-03-15 00:38:52 +0300

    066

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,18 @@
-from django.views.generic import ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
+from orders.forms import OrderForm
 from orders.models import Order
 
 
 class OrderListView(ListView):
     model = Order
+
+
+class OrderCreateView(SuccessMessageMixin,
+                      CreateView):
+    model = Order
+    form_class = OrderForm
+    success_url = reverse_lazy("order_list")
+    success_message = "Заказ добавлен."
