7daf163e629421d9e0fcdf5db8420042e0de73c9
Your Name <you@example.com>
2026-03-15 01:19:51 +0300

    075

--- shoes/urls.py
+++ shoes/urls.py
@@ -24,7 +24,7 @@ from django.conf import settings  # PREP
 from django.conf.urls.static import static  # PREP
 
 from home.views import HomeView
-from orders.views import OrderListView, OrderCreateView, OrderUpdateView
+from orders.views import OrderListView, OrderCreateView, OrderUpdateView, OrderDeleteView
 from products.views import ProductCreateView, ProductUpdateView, ProductDeleteView
 
 urlpatterns = [
@@ -36,6 +36,7 @@ urlpatterns = [
     path('orders/', login_required(OrderListView.as_view()), name='order_list'),
     path('orders/create/', login_required(OrderCreateView.as_view()), name='order_create'),
     path('orders/<int:pk>/update/', login_required(OrderUpdateView.as_view()), name='order_update'),
+    path('orders/<int:pk>/delete/', login_required(OrderDeleteView.as_view()), name='order_delete'),
 ]
 
 urlpatterns += i18n_patterns(
