293025b2fb6e6ed75b5e17855ed4a140eeea81ce
Your Name <you@example.com>
2026-03-15 00:06:19 +0300

    060

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from orders.models import Order
+
+
+class OrderListView(ListView):
+    model = Order
