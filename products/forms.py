3f0eeb1bdedae2e05139b8f11d96dd18e7c02358
Your Name <you@example.com>
2026-03-15 01:29:39 +0300

    078

--- products/forms.py
+++ products/forms.py
@@ -1,9 +1,33 @@
 from concurrency.forms import ConcurrentForm
+from django import forms
 
 from products.models import Product
+from suppliers.models import Supplier
 
 
 class ProductForm(ConcurrentForm):
     class Meta:
         model = Product
-        exclude = ["pk",]
+        exclude = ["pk", ]
+
+
+class SearchSortFilterForm(forms.Form):
+    CHOICES = [
+        ('more', 'Больше'),
+        ('less', 'Меньше'),
+    ]
+
+    stock = forms.ChoiceField(
+        choices=CHOICES,
+        initial='more',
+        required=False,
+        label="Количество на складе"
+    )
+
+    search = forms.CharField(required=False,
+                             label="Поиск")
+
+    supplier = forms.ModelChoiceField(queryset=Supplier.objects.all(),
+                                      empty_label="Все поставщики",
+                                      required=False,
+                                      label="Поставщик")
