8beb1ddb980c7155d270c59a92088b5b0793259f
Your Name <you@example.com>
2026-03-14 22:06:01 +0300

    045

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from products.forms import ProductForm
+from products.models import Product
+
+
+class ProductCreateView(SuccessMessageMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("home")
+    success_message = "Товар добавлен."
