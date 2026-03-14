e59b52847d78337ede789d4e98dfec212d185532
Your Name <you@example.com>
2026-03-14 21:40:12 +0300

    041

--- home/views.py
+++ home/views.py
@@ -1,13 +1,8 @@
-from django.views.generic import TemplateView  # PREP
+from django.views.generic import ListView  # PREP
+
+from products.models import Product
 
 
-class HomeView(TemplateView):  # PREP
-    """
-    PREP
-    Редактировать данный файл по необходимости.
-    При подготовке заготовки проекта надо
-    было вывести на экран какой-то шаблон.
-    Реальная задача может сильно отличаться.
-    """
-
+class HomeView(ListView):  # PREP
+    model = Product
     template_name = "home/home.html"  # PREP
