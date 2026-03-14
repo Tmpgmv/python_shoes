1e8708bfc008a6fab4ea76f580314b691c594657
Your Name <you@example.com>
2026-03-15 01:48:29 +0300

    080

--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.views.generic import ListView  # PREP
 
+from products.forms import SearchSortFilterForm
 from products.models import Product
 
 
 class HomeView(ListView):  # PREP
     template_name = "home/home.html"  # PREP
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        form = SearchSortFilterForm(self.request.GET)
+        context['form'] = form
+        return context
